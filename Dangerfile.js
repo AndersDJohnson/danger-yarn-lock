40e3a6ac8c1ba87ed313eca9b2eede6517714860
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2017-05-06 10:47:16 -0500

    init

---
+++ Dangerfile.js
@@ -0,0 +1,29 @@
+var checkYarkLock = require('./index')
+
+schedule(function (resolved) {
+  checkYarkLock(function (message) {
+    warn(message)
+    resolved()
+  })
+})
+
+// schedule(function (resolved) {
+//   checkYarkLock()
+//     .then(function (message) {
+//       warn(message)
+//       resolved()
+//     })
+// })
+
+// var promise = danger.git.JSONDiffForFile('package.json')
+//   .then(function () {
+//     return 'ok'
+//   })
+//
+// schedule(function (resolved) {
+//   Promise.all([promise, checkYarkLock()])
+//     .then(function (results) {
+//       warn(results[1])
+//       resolved()
+//     })
+// })
---
+++ index.js
@@ -0,0 +1,37 @@
+// http://danger.systems/js/tutorials/dependencies.html
+var includes = require('lodash.includes')
+
+function checkYarnLock(callback) {
+  callback = callback || function () {}
+  return new Promise(function (resolve, reject) {
+    danger.git.JSONDiffForFile('package.json')
+      .then(function (packageDiff) {
+        console.log('packageDiff', packageDiff)
+
+        var hasDependencyChanges = !!(packageDiff.dependencies || packageDiff.devDependencies)
+        console.log('hasDependencyChanges', hasDependencyChanges)
+
+        if (hasDependencyChanges) {
+          var hasLockfileChanges = includes(danger.git.modified_files, 'yarn.lock')
+          console.log('hasLockfileChanges', hasLockfileChanges)
+          if (!hasLockfileChanges) {
+            var message = 'There are "package.json" dependency changes with no "yarn.lock" changes'
+            callback(message)
+            resolve(message)
+          } else {
+            callback()
+            resolve()
+          }
+        } else {
+          callback()
+          resolve()
+        }
+      })
+      .catch(function (err) {
+        callback(err)
+        reject(err)
+      })
+  })
+}
+
+module.exports = checkYarnLock
