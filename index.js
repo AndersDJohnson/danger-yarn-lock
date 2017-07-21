3c1092b8bce7dfca716cf2b2c4f1a69c61c97cfb
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2017-07-21 16:35:43 -0500

    fix: remove console.logs

--- index.js
+++ index.js
@@ -7,14 +7,10 @@ function checkYarnLock (callback) {
   return new Promise(function (resolve, reject) {
     danger.git.JSONDiffForFile('package.json')
       .then(function (packageDiff) {
-        console.log('packageDiff', packageDiff)
-
         var hasDependencyChanges = !!(packageDiff.dependencies || packageDiff.devDependencies)
-        console.log('hasDependencyChanges', hasDependencyChanges)
 
         if (hasDependencyChanges) {
           var hasLockfileChanges = includes(danger.git.modified_files, 'yarn.lock')
-          console.log('hasLockfileChanges', hasLockfileChanges)
           if (!hasLockfileChanges) {
             var message = 'There are "package.json" dependency changes with no "yarn.lock" changes'
             callback(null, message)
